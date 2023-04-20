62af1f1389d5aa80bf296db747674cc52de7ab07
ensonmj <ensonmj@gmail.com>
2023-04-20 13:20:29 +0800

    templates | devcontainer | first add

---
+++ templates/devcontainer/postCreate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -n "$HTTP_PROXY" ]
+then
+    echo "http_proxy=$HTTP_PROXY" >> ~/.wgetrc
+    echo "https_proxy=$HTTP_PROXY" >> ~/.wgetrc
+fi
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# cargo install sccache
+cargo install fd-find
+cargo install ripgrep
+
+sudo apt update
+sudo apt install -y stow
+git clone https://github.com/ensonmj/dotfiles .dotfiles
+pushd ~/.dotfiles
+stow -S zsh
+popd
+sudo apt install -y zsh
