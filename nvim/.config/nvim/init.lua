48d0106617d056e2d78976154714c48003a3d729
ensonmj <ensonmj@gmail.com>
2023-04-22 21:06:27 +0800

    nvim | use Lazyvim

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,5 @@
+if vim.g.vscode then
+else
+	-- bootstrap lazy.nvim, LazyVim and other plugins
+	require("config.lazy")
+end
---
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -0,0 +1,6 @@
+return {
+    -- a lot of plugins that support versioning, have outdated releases,
+    -- which may break your Neovim install.
+    { "folke/lazy.nvim", version = false },
+    { "LazyVim/LazyVim", version = false },
+}
