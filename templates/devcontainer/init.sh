df4742e2cf01999f641cfe34cc2eb63aea0b2a78
ensonmj <ensonmj@gmail.com>
2023-04-27 22:14:40 +0800

    templates | devcontainer | ARG can't be modified

--- templates/devcontainer/init.sh
+++ templates/devcontainer/init.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# this file used for init local enviroment before container is build.
-
-export DISPLAY=$DISPLAY
-export HTTP_PROXY_HOST=$HTTP_PROXY_HOST
-export HTTP_PROXY_PORT=$HTTP_PROXY_PORT
-export STORAGE=
-
-# guard to make sure you modified this file firstly
+# some local environments used in devcontainer.json,
+# we should check them before starting to build container.
+# cuation: you can't set them in this file.
 test -n "$DISPLAY" || (echo "DISPLAY not set" && false)
 test -n "$HTTP_PROXY_HOST" || (echo "HTTP_PROXY_HOST not set" && false)
 test -n "$HTTP_PROXY_PORT" || (echo "HTTP_PROXY_PORT not set" && false)
 test -n "$STORAGE" || (echo "STORAGE not set" && false)
-
