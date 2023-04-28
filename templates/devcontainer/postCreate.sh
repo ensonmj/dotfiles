f02e35eedc3cd226172041f137c7affc9a95d6c9
ensonmj <ensonmj@gmail.com>
2023-04-28 11:25:49 +0800

    templates | devcontainer | support mount this directory to container

--- templates/devcontainer/postCreate.sh
+++ templates/devcontainer/postCreate.sh
@@ -9,11 +9,12 @@ then
     echo "https_proxy=$HTTP_PROXY" >> ~/.wgetrc
 fi
 
-# pushd ~/.dotfiles
-# stow -S zsh
-# stow -S profile
-# stow -S bash
-# popd
+[[ -d ~/.dotfiles ]] || git clone https://github.com/ensonmj/dotfiles .dotfiles
+pushd ~/.dotfiles
+stow -S zsh
+stow -S profile
+stow -S bash
+popd
 
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> ~/.bashrc
 echo "export PATH=$JAVA_HOME/bin:$PATH" >> ~/.bashrc
@@ -47,10 +48,47 @@ curl -s "https://get.sdkman.io" | bash
 # sed -i -e 's/sdkman_auto_answer=false/sdkman_auto_answer=true/g' $HOME/.sdkman/etc/config
 sdk install sbt
 
+# for gluten
+sudo apt install -y libiberty-dev libdwarf-dev libre2-dev \
+    libz-dev libssl-dev libboost-all-dev \
+    libcurl4-openssl-dev libjemalloc-dev
+
 # big storage
 [[ -d ./storage ]] || ln -s /storage storage
 # dependencies
 pushd  storage
+
+# # spark 3.2.2
+# git clone --depth 1 --branch v3.2.2 https://github.com/apache/spark.git spark322
+# pushd spark322
+# ./build/mvn -Pyarn -DskipTests clean install
+# popd
+
+# # spark 3.3.0
+# # git clone --depth 1 --branch v3.3.0 https://github.com/apache/spark.git spark330
+# # pushd spark330
+# # ./build/mvn -Pyarn -DskipTests clean install
+# # popd
+
+# # spark-sql-perf
+# git clone --depth 1 https://github.com/databricks/spark-sql-perf.git
+# pushd spark-sql-perf
+# sbt +package
+# popd
+
+# # tpch-dbgen
+# git clone https://github.com/databricks/tpch-dbgen
+# pushd tpch-dbgen
+# git checkout 0469309147b42abac8857fa61b4cf69a6d3128a8
+# make clean && make
+# popd
+
+# # tpcds-kit
+# git clone https://github.com/databricks/tpcds-kit
+# pushd tpcds-kit/tools
+# sudo apt-get -y install gcc make flex bison byacc git
+# make clean && make
+# popd
 
 popd
 
