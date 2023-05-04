5acee8db9e25ab4df5e97d180024ae84d716fdea
ensonmj <ensonmj@gmail.com>
2023-05-04 23:47:33 +0800

    install | add some utils and nerd fonts script

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-cargo install fd-find
-cargo install ripgrep
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
+source ~/.cargo/env
+# cargo install sccache
+cargo install --locked bat
+cargo install --locked bottom
+cargo install --locked delta
+cargo install --locked fd-find
+cargo install --locked hyperfine
+cargo install --locked just
+cargo install --locked miniserve
+cargo install --locked procs
+cargo install --locked ripgrep
+#cargo install --locked starship # need to config shell and nerd fonts
+cargo install --locked tokei
+# cargo install --locked zoxide # need to config shell
