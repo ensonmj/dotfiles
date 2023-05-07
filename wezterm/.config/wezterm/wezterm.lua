96ef6ff6678a386acb498a7e3619fed348d9f51e
ensonmj <ensonmj@gmail.com>
2023-05-07 23:54:22 +0800

    wezterm | bordless windown

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -12,18 +12,24 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
-config.color_scheme = "Batman"
+config.color_scheme = "Dracula (Official)"
 config.font = wezterm.font_with_fallback({ "FiraMono Nerd Font Mono", "monospace", monospace })
 config.font_size = 11 
 -- config.line_height = 0.9
 -- config.cell_width = 0.9
+config.scrollback_lines = 99999
+config.launch_menu = require("launch_menu")
+
 -- Window
+config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.98 -- 如果设置为1.0会明显卡顿
 config.window_background_image_hsb = {
   hue = 1.0,
   saturation = 1.0,
   brightness = 0.8,
 }
+
+-- Pane
 config.inactive_pane_hsb = {
   hue = 1.0,
   saturation = 1.0,
@@ -31,8 +37,9 @@ config.inactive_pane_hsb = {
 }
 
 -- Tab bar
-config.hide_tab_bar_if_only_one_tab = true
-config.scrollback_lines = 99999
+-- config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
 
 -- Keys
 -- config.disable_default_key_bindings = false
@@ -40,8 +47,9 @@ config.scrollback_lines = 99999
 config.use_dead_keys = false
 config.leader = { key="a", mods="CTRL" }
 config.keys = require("keys")
+
+-- Mouse
 config.mouse_bindings = require("mouse")
-config.launch_menu = require("launch_menu")
 
 -- and finally, return the configuration to wezterm
 return config
