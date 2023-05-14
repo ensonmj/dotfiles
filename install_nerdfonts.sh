2aaeef714f07acea5b8a04ae117c3e46b7362a21
ensonmj <ensonmj@gmail.com>
2023-05-14 11:38:22 +0800

    nerdfonts | add sarasa fonts

--- install_nerdfonts.sh
+++ install_nerdfonts.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 # https://gist.github.com/matthewjberger/7dd7e079f282f8138a9dc3b045ebefa0
 
-declare -a fonts=(
+fonts_dir="${HOME}/.local/share/fonts"
+if [[ ! -d "$fonts_dir" ]]; then
+    mkdir -p "$fonts_dir"
+fi
+
+nerd_ver='3.0.0'
+declare -a nerd_fonts=(
     #BitstreamVeraSansMono
+    CascadiaCode
     CodeNewRoman
+    # DejavuSansMono
     DroidSansMono
     FiraCode
     FiraMono
-    #Go-Mono
-    #Hack
-    #Hermit
+    IBMPlexMono
+    Inconsalata
+    Iosevka
+    IosevkaTerm
     JetBrainsMono
     LiberationMono
     Meslo
     Monoid
     Mononoki
+    # NerdFontsSymbolsOnly
     Noto
-    #Overpass
-    #ProggyClean
     RobotoMono
     SourceCodePro
     SpaceMono
@@ -25,17 +33,29 @@ declare -a fonts=(
     Ubuntu
     UbuntuMono
 )
-
-version='3.0.0'
-fonts_dir="${HOME}/.local/share/fonts"
-
-if [[ ! -d "$fonts_dir" ]]; then
-    mkdir -p "$fonts_dir"
-fi
-
-for font in "${fonts[@]}"; do
+for font in "${nerd_fonts[@]}"; do
     zip_file="${font}.zip"
-    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
+    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${nerd_ver}/${zip_file}"
+    echo "Downloading $download_url"
+    wget "$download_url"
+    unzip -uo "$zip_file" -d "$fonts_dir"
+    rm "$zip_file"
+done
+
+sarasa_ver='0.40.7-0'
+declare -a sarasa_fonts=(
+    # sarasa-fixed-sc-nerd-font
+    # sarasa-fixed-slab-sc-nerd-font
+    sarasa-gothic-sc-nerd-font
+    sarasa-mono-sc-nerd-font
+    # sarasa-mono-slab-sc-nerd-font
+    sarasa-term-sc-nerd-font
+    # sarasa-term-slab-sc-nerd-font
+    sarasa-ui-sc-nerd-font
+)
+for font in "${sarasa_fonts[@]}"; do
+    zip_file="${font}.zip"
+    download_url="https://github.com/jonz94/Sarasa-Gothic-Nerd-Fonts/releases/download/v${sarasa_ver}/${zip_file}"
     echo "Downloading $download_url"
     wget "$download_url"
     unzip -uo "$zip_file" -d "$fonts_dir"
