d77589fa8cd340c538d5294fa5ba2ea18251c732
ensonmj <ensonmj@gmail.com>
2023-05-23 20:50:41 +0800

    wezterm | right mouse copy and paste

--- wezterm/.config/wezterm/mouse.lua
+++ wezterm/.config/wezterm/mouse.lua
@@ -1,41 +1,33 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 return {
-  -- Paste on right-click
-  {
-    event = {
-      Down = {
-        streak = 1,
-        button = "Right",
-      },
-    },
-    mods = "NONE",
-    action = wezterm.action({
-      PasteFrom = "Clipboard",
-    }),
-  },
   -- Change the default click behavior so that it only selects
   -- text and doesn't open hyperlinks
   {
-    event = {
-      Up = {
-        streak = 1,
-        button = "Left",
-      },
-    },
+    event = { Up = { streak = 1, button = "Left" } },
     mods = "NONE",
-    action = wezterm.action({
+    action = act({
       CompleteSelection = "PrimarySelection",
     }),
   },
+  -- select to copy(not wezterm copy mode), and paste if don't select anything
+  {
+    event = { Down = { streak = 1, button = "Right" } },
+    mods = "NONE",
+    action = wezterm.action_callback(function(window, pane)
+      local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+      if has_selection then
+        window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+        window:perform_action(act.ClearSelection, pane)
+      else
+        window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+      end
+    end),
+  },
   -- CTRL-Click open hyperlinks
   {
-    event = {
-      Up = {
-        streak = 1,
-        button = "Left",
-      },
-    },
+    event = { Up = { streak = 1, button = "Left" } },
     mods = "CTRL",
     action = "OpenLinkAtMouseCursor",
   },
