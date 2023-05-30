bb932e0ce3bb5a592f9982a889970d1396898954
ensonmj <ensonmj@gmail.com>
2023-05-30 21:53:57 +0800

    install | add gitui

--- install.sh
+++ install.sh
@@ -82,8 +82,8 @@ popd
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y \
     --no-modify-path --default-toolchain stable --profile default
 source ~/.cargo/env
-cargo install --locked bat bottom erdtree git-delta hyperfine just miniserve \
-    procs ripgrep starship tokei zoxide
+cargo install bat bottom erdtree git-delta gitui hyperfine just \
+    miniserve procs ripgrep starship tokei zoxide
 # cargo install --git https://github.com/mmstick/parallel
 # parallel cargo install --locked ::: bat bottom erdtree git-delta hyperfine \
 #     just miniserve procs ripgrep starship tokei zoxide
