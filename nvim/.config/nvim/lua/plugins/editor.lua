6af9e0c6644e3784a4014d9f6451d1ddeab499cc
ensonmj <ensonmj@gmail.com>
2023-06-20 09:30:16 +0800

    nvim | add toggleterm

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,4 +1,37 @@
 return {
+  -- toggleterm
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup({
+        open_mapping = [[<c-\>]],
+        shade_terminals = false,
+        -- add --login so ~/.zprofile is loaded
+        -- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
+        shell = "zsh --login",
+      })
+    end,
+    keys = {
+      { [[<C-\>]] },
+      {
+        "<leader>tv",
+        function()
+          local count = vim.v.count1
+          require("toggleterm").toggle(count, 0, vim.loop.cwd(), "vertical")
+        end,
+        desc = "ToggleTerm (vertical)",
+      },
+      {
+        "<leader>th",
+        function()
+          local count = vim.v.count1
+          require("toggleterm").toggle(count, 10, vim.loop.cwd(), "horizontal")
+        end,
+        desc = "ToggleTerm (horizontal)",
+      },
+    },
+  },
+
   -- disable mini.bufremove
   { "echasnovski/mini.bufremove", enabled = false },
 
