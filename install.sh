19392cf18b281c16451dfaf0b29bb02fe182d4d9
ensonmj <ensonmj@gmail.com>
2023-07-24 20:55:59 +0800

    templates | devcontainer | cargo installation acceleration

--- install.sh
+++ install.sh
@@ -12,46 +12,46 @@ export XDG_DATA_HOME=${XDG_DATA_HOME:=$HOME/.local/share}
 export XDG_CACHE_HOME=${XDG_CACHE_HOME:=$HOME/.cache}
 
 # https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 source ${SCRIPT_DIR}/install_nerdfonts.sh
 
 # Install stow (if not already installed)
 if [ -f /etc/os-release ]; then
-    # freedesktop.org and systemd
-    . /etc/os-release
-    OS=$NAME
-    VER=$VERSION_ID
+	# freedesktop.org and systemd
+	. /etc/os-release
+	OS=$NAME
+	VER=$VERSION_ID
 elif type lsb_release >/dev/null 2>&1; then
-    # linuxbase.org
-    OS=$(lsb_release -si)
-    VER=$(lsb_release -sr)
+	# linuxbase.org
+	OS=$(lsb_release -si)
+	VER=$(lsb_release -sr)
 elif [ -f /etc/lsb-release ]; then
-    # For some versions of Debian/Ubuntu without lsb_release command
-    . /etc/lsb-release
-    OS=$DISTRIB_ID
-    VER=$DISTRIB_RELEASE
+	# For some versions of Debian/Ubuntu without lsb_release command
+	. /etc/lsb-release
+	OS=$DISTRIB_ID
+	VER=$DISTRIB_RELEASE
 elif [ -f /etc/debian_version ]; then
-    # Older Debian/Ubuntu/etc.
-    OS=Debian
-    VER=$(cat /etc/debian_version)
+	# Older Debian/Ubuntu/etc.
+	OS=Debian
+	VER=$(cat /etc/debian_version)
 elif [ -f /etc/redhat-release ]; then
-    # Older Red Hat, CentOS, etc.
-    ...
+	# Older Red Hat, CentOS, etc.
+	...
 else
-    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
-    OS=$(uname -s)
-    VER=$(uname -r)
+	# Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+	OS=$(uname -s)
+	VER=$(uname -r)
 fi
 
 case $OS in
-  *Ubuntu*)
-    dpkg -s stow &> /dev/null || sudo apt-get -y install stow
-    dpkg -s cmake &> /dev/null || sudo apt-get -y install cmake
-    ;;
-  *Arch*)
-    pacman -Q stow &> /dev/null || yes | sudo pacman -S stow
-    pacman -Q cmake &> /dev/null || yes | sudo pacman -S cmake
-    ;;
+*Ubuntu*)
+	dpkg -s stow &>/dev/null || sudo apt-get -y install stow
+	dpkg -s cmake &>/dev/null || sudo apt-get -y install cmake
+	;;
+*Arch*)
+	pacman -Q stow &>/dev/null || yes | sudo pacman -S stow
+	pacman -Q cmake &>/dev/null || yes | sudo pacman -S cmake
+	;;
 esac
 
 # Stow packages {{{
@@ -67,32 +67,35 @@ pushd $SCRIPT_DIR
 [[ -f ~/.zshrc ]] && mv ~/.zshrc{,.orig}
 # Just install some configs
 declare -a confs=(
-    stow
-    dircolors
-    starship
-    readline
-    profile
-    bash
-    zsh
-    vim
-    nvim
-    wezterm
+	stow
+	dircolors
+	starship
+	readline
+	profile
+	bash
+	zsh
+	vim
+	nvim
+	wezterm
 )
 for conf in "${confs[@]}"; do
-    stow -S "$conf"
+	stow -S "$conf"
 done
 popd
 # }}}
 
 echo "Start to install some command line tools, $(date)"
-if ! command -v cargo &> /dev/null; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y \
-    --no-modify-path --default-toolchain stable --profile default
-    if [ $? -ne 0 ]; then
-        RED='\033[0;31m'
-        NC='\033[0m' # No Color
-        echo -e "${RED}Install rustup failed${NC}"; exit 1
-    fi
+if ! command -v cargo &>/dev/null; then
+	export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
+	export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y \
+		--no-modify-path --default-toolchain stable --profile default
+	if [ $? -ne 0 ]; then
+		RED='\033[0;31m'
+		NC='\033[0m' # No Color
+		echo -e "${RED}Install rustup failed${NC}"
+		exit 1
+	fi
 fi
 source ~/.cargo/env
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
@@ -100,46 +103,47 @@ curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-
 # cargo install cargo-quickinstall
 # https://gist.github.com/sts10/daadbc2f403bdffad1b6d33aff016c0a
 declare -a bins=(
-    bandwhich
-    bat
-    bottom
-    broot
-    dufs
-    du-dust
-    erdtree
-    fd-find
-    git-delta
-    gitui
-    hyperfine
-    just
-    procs
-    ripgrep
-    # sccache # depends on pkg-config
-    sd
-    skim
-    starship # need to config shell and install nerd fonts
-    stylua
-    tealdeer
-    topgrade
-    tokei
-    xcp
-    zoxide # need to config shell
+	bandwhich
+	bat
+	bottom
+	broot
+	dufs
+	du-dust
+	erdtree
+	fd-find
+	git-delta
+	gitui
+	hyperfine
+	just
+	procs
+	ripgrep
+	# sccache # depends on pkg-config
+	sd
+	skim
+	starship # need to config shell and install nerd fonts
+	stylua
+	tealdeer
+	topgrade
+	tokei
+	xcp
+	zoxide # need to config shell
 )
 # install in parallel and wait until finish
 # https://klotzandrew.com/blog/parallel-bash-with-wait
+# cargo binstall --no-confirm --no-symlinks "${bins[@]}"
 declare -A status
 for bin in "${bins[@]}"; do
-    #cargo quickinstall "$bin" &
-    cargo binstall --no-confirm --no-symlinks "$bin" &
-    # pids+=($!)
-    status["$bin"]=$!
+	#cargo quickinstall "$bin" &
+	cargo binstall --no-confirm --no-symlinks "$bin" &
+	# pids+=($!)
+	status["$bin"]=$!
 done
 for bin in "${!status[@]}"; do
-    wait "${status[$bin]}"
-    status["$bin"]=$?
+	wait "${status[$bin]}"
+	status["$bin"]=$?
 done
 for bin in "${!status[@]}"; do
-    echo "install $bin exited with ${status[$bin]}"
+	echo "install $bin exited with ${status[$bin]}"
 done
 echo "All done, $(date)"
 
