e3cba521d693ef12232c833a03592a0015779376
ensonmj <ensonmj@gmail.com>
2023-07-27 16:38:44 +0800

    nvim | add guess-indent

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,4 +1,12 @@
 return {
+  -- guess indent
+  {
+    "nmac427/guess-indent.nvim",
+    vscode = true,
+    config = function()
+      require("guess-indent").setup({})
+    end,
+  },
   -- toggleterm
   {
     "akinsho/toggleterm.nvim",
@@ -38,25 +46,24 @@ return {
   -- use bdelete instead
   {
     "famiu/bufdelete.nvim",
-      -- stylua: ignore
-      config = function()
-        -- switches to Alpha dashboard when last buffer is closed
-        local alpha_on_empty = vim.api.nvim_create_augroup("alpha_on_empty", { clear = true })
-        vim.api.nvim_create_autocmd("User", {
-          pattern = "BDeletePost*",
-          group = alpha_on_empty,
-          callback = function(event)
-            local fallback_name = vim.api.nvim_buf_get_name(event.buf)
-            local fallback_ft = vim.api.nvim_buf_get_option(event.buf, "filetype")
-            local fallback_on_empty = fallback_name == "" and fallback_ft == ""
-            if fallback_on_empty then
-              require("neo-tree").close_all()
-              vim.cmd("Alpha")
-              vim.cmd(event.buf .. "bwipeout")
-            end
-          end,
-        })
-      end,
+    config = function()
+      -- switches to Alpha dashboard when last buffer is closed
+      local alpha_on_empty = vim.api.nvim_create_augroup("alpha_on_empty", { clear = true })
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "BDeletePost*",
+        group = alpha_on_empty,
+        callback = function(event)
+          local fallback_name = vim.api.nvim_buf_get_name(event.buf)
+          local fallback_ft = vim.api.nvim_buf_get_option(event.buf, "filetype")
+          local fallback_on_empty = fallback_name == "" and fallback_ft == ""
+          if fallback_on_empty then
+            require("neo-tree").close_all()
+            vim.cmd("Alpha")
+            vim.cmd(event.buf .. "bwipeout")
+          end
+        end,
+      })
+    end,
     keys = {
       { "<leader>bd", "<CMD>Bdelete<CR>", desc = "Delete Buffer" },
       { "<leader>bD", "<CMD>Bdelete!<CR>", desc = "Delete Buffer (Force)" },
