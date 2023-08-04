40ab3689036440fae8536c26a825367aeaaea862
ensonmj <ensonmj@gmail.com>
2023-08-04 16:54:02 +0800

    install | mv cargo to install_tools

--- install_tools.sh
+++ install_tools.sh
@@ -2,6 +2,32 @@
 
 echo "Start to install some command line tools, $(date)"
 
+if ! command -v cargo &>/dev/null; then
+	export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
+	export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y \
+		--no-modify-path --default-toolchain stable --profile default
+	if [ $? -ne 0 ]; then
+		RED='\033[0;31m'
+		NC='\033[0m' # No Color
+		echo -e "${RED}Install rustup failed${NC}"
+		exit 1
+	fi
+fi
+source ~/.cargo/env
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+# cargo install cargo-binstall
+# cargo install cargo-quickinstall
+# https://gist.github.com/sts10/daadbc2f403bdffad1b6d33aff016c0a
+declare -a bins=(
+	fd-find
+	git-delta
+	ripgrep
+	starship # need to config shell and install nerd fonts
+	zoxide # need to config shell
+)
+cargo binstall --no-confirm --no-symlinks "${bins[@]}"
+
 declare -a tools=(
 	bandwhich
 	bat
@@ -24,7 +50,6 @@ declare -a tools=(
 	xcp
 )
 
-source ~/.cargo/env
 declare -A status
 for bin in "${tools[@]}"; do
 	#cargo quickinstall "$bin" &
